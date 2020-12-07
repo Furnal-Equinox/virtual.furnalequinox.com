6d04937bfdb377e7ba63eb5d36ec87b5deef2256
William Howell <wlm.howell@gmail.com>
2020-12-06 19:28:44 -0500

    Copy the styling from the live data cards over to the mock  cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -18,9 +18,14 @@ const Card: React.FC<Props> = ({ title, dealer, description, banner, slug }: Pro
       <div className="card mb-5 shadow-sm">
         <img src={banner} className='card-img-top'></img>
         <div className="card-body">
-          <p className="card-text text-center">
-            {description}
-          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <p className='m-0'>
+              {`${title} by ${dealer}`}  
+            </p>
+            <p className="card-text text-center">
+              {description}
+            </p>
+          </div>
         </div>
       </div>
     </Link>
