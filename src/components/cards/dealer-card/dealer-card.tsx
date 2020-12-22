2525b37cab05cf847029477c443585488ebcb851
William Howell <wlm.howell@gmail.com>
2020-12-22 08:42:59 -0500

    Replaced internal card and external card with a new Link component to aid accessibility

--- src/components/cards/dealer-card/dealer-card.tsx
+++ src/components/cards/dealer-card/dealer-card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.scss'
 
-import ClickableInternalCard from '../clickable-internal-card/clickable-internal-card'
+import Link from '../../link/link'
 
 interface Dealer {
   title: string
@@ -15,21 +15,20 @@ type Props = Dealer
 
 const DealerCard: React.FC<Props> = ({ title, dealer, description, banner, slug }: Props) => {
   return (
-    <ClickableInternalCard slug={`.${slug}`}>
-      <div className='card rounded-card mb-5 shadow-sm'>
-        <img src={banner} className='card-img-top rounded-card-img-top'></img>
-        <div className='card-body'>
-          <div className='d-flex justify-content-between align-items-center'>
-            <p className='m-0'>
-              {`${title} by ${dealer}`}  
-            </p>
-            <p className='card-text text-center'>
-              {description}
-            </p>
-          </div>
+    <div className='card rounded-card mb-5 shadow-sm'>
+      <img src={banner} className='card-img-top rounded-card-img-top'></img>
+      <div className='card-body'>
+        <div className='d-flex justify-content-between align-items-center'>
+          <p className='m-0'>
+            {`${title} by ${dealer}`}  
+          </p>
+          <p className='card-text text-center'>
+            {description}
+          </p>
+          <Link to={`.${slug}`} label='Go' />
         </div>
       </div>
-    </ClickableInternalCard>
+    </div>
   )
 }
 
---
+++ src/components/link/link.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import GatsbyLink from 'gatsby-link'
+
+export interface Props {
+  label: string
+  to: string
+  state?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link'
+  hasOutline?: boolean
+  isFullwidth?: boolean
+  size?: 'sm' | 'lg'
+}
+
+const Anchor: React.FC<Props> = ({ 
+  state = undefined, 
+  hasOutline = false, 
+  size = undefined, 
+  isFullwidth = false, 
+  label, 
+  to, 
+  ...props 
+}: Props) => {
+  return (
+    <div className={isFullwidth ? 'd-grid' : undefined}>
+      <GatsbyLink
+        to={to}
+        className={[
+          'btn rounded-pill',
+          `btn${hasOutline ? '-outline' : ''}${state !== undefined ? `-${state}` : '-primary'}`,
+          `btn${size !== undefined ? `-${size}` : ''}`
+        ].join(' ')}
+        {...props}
+      >
+        {label}
+      </GatsbyLink>
+    </div>
+  )
+}
+
+export default Anchor
