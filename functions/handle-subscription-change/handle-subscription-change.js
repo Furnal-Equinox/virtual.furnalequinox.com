86f52c935859c11b2350d6728d45141a6e0cda47
William Howell <wlm.howell@gmail.com>
2021-01-15 21:00:39 -0500

    how about this?!?!

--- functions/handle-subscription-change/handle-subscription-change.js
+++ functions/handle-subscription-change/handle-subscription-change.js
@@ -1,6 +1,24 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const fetch = require('node-fetch')
-const { faunaFetch } = require('./utils/fauna')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const faunaFetch = async ({ query, variables }) => {
+  return await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+    .then(
+      (res) => res.json()
+    )
+    .catch(
+      (err) => console.error(JSON.stringify(err, null, 2))
+    )
+}
 
 const handler = async ({ body, headers }, context) => {
   try {
